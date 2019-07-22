ccc77a33ecd2968ccc82e7666614124831e37bf2
Matias Cejas <matiascejas8@hotmail.com>
2019-07-22 13:42:12 -0300

    conectado a la api

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import moment from 'moment';
 class App extends Component {
   state = {
     lat: undefined,
-    lon: undefined,  
+    lon: undefined,
     city: undefined,
     temperatureC: undefined,
     temperatureF: undefined,
@@ -18,12 +18,12 @@ class App extends Component {
   getPosition = () => {
     return new Promise(function (resolve, reject) {
       navigator.geolocation.getCurrentPosition(resolve, reject);
-    });    
+    });
   }
-  
-  getWeather = async (latitude, longitude) => { 
+
+  getWeather = async (latitude, longitude) => {
     const api_call = await fetch(`//api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`);
-    const data = await api_call.json();    
+    const data = await api_call.json();
     this.setState({
       lat: latitude,
       lon: longitude,
@@ -38,16 +38,16 @@ class App extends Component {
 
   componentDidMount() {
     this.getPosition()
-    .then((position) => {      
-      this.getWeather(position.coords.latitude, position.coords.longitude)
-    })
-    .catch((err) => {
-      this.setState({ errorMessage: err.message });
-    });
+      .then((position) => {
+        this.getWeather(position.coords.latitude, position.coords.longitude)
+      })
+      .catch((err) => {
+        this.setState({ errorMessage: err.message });
+      });
 
-    this.timerID = setInterval(        
-      () => 
-      this.getWeather(this.state.lat, this.state.lon),
+    this.timerID = setInterval(
+      () =>
+        this.getWeather(this.state.lat, this.state.lon),
       60000
     );
   }
@@ -56,17 +56,17 @@ class App extends Component {
     clearInterval(this.timerID);
   }
 
-  render() {    
-    const { city, temperatureC, temperatureF, icon, sunrise, sunset} = this.state;
+  render() {
+    const { city, temperatureC, temperatureF, icon, sunrise, sunset } = this.state;
     if (city) {
       return (
-        <div className="App">  
+        <div className="App">
           <div className="weather-box">
-            <div className="weather-item">{city}</div>      
-            <div className="weather-item">{temperatureC} &deg;C <span className="slash">/</span> {temperatureF} &deg;F</div>    
+            <div className="weather-item">{city}</div>
+            <div className="weather-item">{temperatureC} &deg;C <span className="slash">/</span> {temperatureF} &deg;F</div>
             <div>
-              <img className="weather-icon" src={`http://openweathermap.org/img/w/${icon}.png`} alt="weather icon"/>
-            </div>    
+              <img className="weather-icon" src={`http://openweathermap.org/img/w/${icon}.png`} alt="weather icon" />
+            </div>
             <div className="weather-item">
               <span>sunrise: {sunrise}</span>
             </div>
@@ -75,13 +75,13 @@ class App extends Component {
             </div>
           </div>
         </div>
-      );      
+      );
     }
     else {
       return (
         <div>Loading...</div>
       )
-    }    
+    }
   }
 }
 

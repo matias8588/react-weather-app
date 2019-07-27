0c59cdf317f0b4246753ffa18961377a09c1e217
Matias Cejas <matiascejas8@hotmail.com>
2019-07-27 18:00:25 -0300

    Updates

---
+++ precache-manifest.b0e1d511ba1b4ac018359aed8005b589.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "afdef20da40beccdea2f38adf5891cfb",
+    "url": "/react-weather-app/static/media/rain.afdef20d.jpg"
+  },
+  {
+    "revision": "803ff5aa6c64abb49d547cd17db4466a",
+    "url": "/react-weather-app/static/media/clear.803ff5aa.jpg"
+  },
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  },
+  {
+    "revision": "4cc7befd5965edbdf095",
+    "url": "/react-weather-app/static/js/main.e4395884.chunk.js"
+  },
+  {
+    "revision": "dbdaebf477a8e1f935b6",
+    "url": "/react-weather-app/static/js/2.0579c50a.chunk.js"
+  },
+  {
+    "revision": "4cc7befd5965edbdf095",
+    "url": "/react-weather-app/static/css/main.ba479531.chunk.css"
+  },
+  {
+    "revision": "dbdaebf477a8e1f935b6",
+    "url": "/react-weather-app/static/css/2.cccbff56.chunk.css"
+  },
+  {
+    "revision": "a1db7de38ab90609f3c3de47e0bd25a8",
+    "url": "/react-weather-app/index.html"
+  }
+];
